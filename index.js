c39a1ccdef50847734f61e31295572de9799bcdc
yeasin44 <mohammadyeasinchy44@gmail.com>
2022-11-10 03:45:04 +0600

    changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
       const alLServices = await cursor.toArray();
       res.send(alLServices);
     });
-    app.get("/services/:id", async (req, res) => {
+    app.get("/allServices/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await serviceCollection.findOne(query);
