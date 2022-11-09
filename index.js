fcf67127bd2085b7a729f8a1f52c1c7325b73b30
yeasin44 <mohammadyeasinchy44@gmail.com>
2022-11-10 00:08:02 +0600

    service added

--- index.js
+++ index.js
@@ -1,40 +1,83 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const services = require("./Data/services.json");
 // /////////////////Database start////////////////////////////
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g5zarfc.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("travelSphere").collection("services");
+    const reviewCollection = client.db("travelSphere").collection("reviews");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // //////Review API///////////////
+
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+
+    // Delete method
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Add services
+
+    app.post("/addService", async (req, res) => {
+      const addService = req.body;
+      const result = await serviceCollection.insertOne(addService);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
 
 // ////////////////////////////////////////////////////////////////////////////////
-
-app.get("/services", (req, res) => {
-  res.send(services);
-});
-
-app.get("/services/:id", (req, res) => {
-  const id = req.params.id;
-  const allServices = services.find((service) => service._id === id);
-  res.send(allServices);
-});
 
 app.get("/", (req, res) => {
   res.send("Travelsphere server is running");
